4a9b2baaa410b9e3f559d757cbf1d221e9a4de0a
Arnim <mail@arnim-antritter.de>
2022-04-24 20:02:07 +0200

    Karten-Klasse hinzugefügt

---
+++ Karte.java
@@ -0,0 +1,46 @@
+/**
+ * Repräsentiert eine Spielkarte.
+ * @author Arnim Antritter
+ * @version 2022.4.24
+ */
+public class Karte extends Button {
+    private int wert;
+    private boolean spielbar;
+    
+    /**
+     * Erzeugt eine neue Karte.
+     * @param x x-Koordinate
+     * @param y y-Koordinate
+     * @param wert der Wert der Karte
+     */
+    public Karte(int x, int y, int wert) {
+        super(x, y);
+        this.wert = wert;
+        setzeSpielbar(true);
+    }
+    
+    /**
+     * Ändert das Aussehen der Karte.
+     * @param spielbar ob die Karte abgelegt werden kann
+     */
+    public void setzeSpielbar(boolean spielbar) {
+        this.spielbar = spielbar;
+        setImage("Karte/" + wert + "/" + (spielbar ? "bunt" : "grau") + ".png");
+    }
+    
+    /**
+     * Gibt zurück, ob die Karte gelegt werden kann.
+     * @return ob die Karte gelegt werden kann.
+     */
+    public boolean istSpielbar() {
+        return spielbar;
+    }
+    
+    /**
+     * Wird aufgerufen, wenn die Karte angeklickt wird.
+     */
+    void onClick() {
+        if (!spielbar) return;
+        //TODO: Karte ablegen
+    }
+}
